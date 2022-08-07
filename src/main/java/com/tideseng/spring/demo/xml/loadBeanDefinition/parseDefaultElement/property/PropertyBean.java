bbcb7d39c8c2dccb678eae9eb09624ed7460653a
sedi <justdoit_onebyone@163.com>
2022-08-07 22:16:56 +0800

    Spring 解析Property标签

---
+++ src/main/java/com/tideseng/spring/demo/xml/loadBeanDefinition/parseDefaultElement/property/PropertyBean.java
@@ -0,0 +1,12 @@
+package com.tideseng.spring.demo.xml.loadBeanDefinition.parseDefaultElement.property;
+
+import lombok.Data;
+
+@Data
+public class PropertyBean {
+
+    private String username;
+
+    private String password;
+
+}
