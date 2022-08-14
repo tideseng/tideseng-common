5b13a80aa66eb65ed5582bc3c236180edd4f5051
sedi <justdoit_onebyone@163.com>
2022-08-14 20:32:21 +0800

    Spring 解析property-placeholder自定义标签

---
+++ src/main/java/com/tideseng/spring/demo/xml/loadBeanDefinition/parseCustomElement/propertyPlaceholder/PropertyPlaceholderBean.java
@@ -0,0 +1,16 @@
+package com.tideseng.spring.demo.xml.loadBeanDefinition.parseCustomElement.propertyPlaceholder;
+
+import lombok.Data;
+
+/**
+ * @author jiahuan
+ * @create 2022/8/1
+ */
+@Data
+public class PropertyPlaceholderBean {
+
+    private String username;
+
+    private String password;
+
+}
